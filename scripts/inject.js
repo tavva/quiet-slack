6debc220051014bd6aa975b168a13f3e45bac253
Ben Phillips <ben@ben-phillips.net>
2019-10-05 10:56:24 +0100

    Wire up button.

--- scripts/inject.js
+++ scripts/inject.js
@@ -1,12 +1,19 @@
 window.addEventListener ("load", main, false);
 
-var show_hide_button = document.createElement('div');
+var show_hide_button = document.createElement('button');
 show_hide_button.appendChild(document.createTextNode('Show channels'));
 show_hide_button.classList.add('show-hide-button');
 
-function main (evt) {
-  var insert_before_node = document.getElementsByClassName('p-channel_sidebar__list')[0];
-  var parent_node = insert_before_node.parentNode;
+var hidden = true;
 
-  parent_node.insertBefore(show_hide_button, insert_before_node);
+function main (evt) {
+  var sidebar_node = document.getElementsByClassName('p-channel_sidebar__list')[0];
+
+  show_hide_button.addEventListener('click', function (evt) {
+    sidebar_node.style.visibility = hidden ? 'visible' : 'hidden';
+    show_hide_button.innerHTML = hidden ? 'Hide channels' : 'Show channels';
+    hidden = !hidden;
+  });
+
+  sidebar_node.parentNode.insertBefore(show_hide_button, sidebar_node);
 }
