59a8702f910013de53356281e6b4a0443244f214
Ben Phillips <ben@thread.com>
2019-10-07 17:23:03 +0100

    Show/hide unread messages in search results.

--- scripts/inject.js
+++ scripts/inject.js
@@ -7,12 +7,32 @@ show_hide_button.classList.add('c-button-unstyled');
 
 var hidden = true;
 
+function inject_css(str) {
+  var node = document.createElement('style');
+  node.innerHTML = str;
+  document.body.appendChild(node);
+}
+
+selectors = {
+  'Unread search results header': function(value) {return `div.c-search_autocomplete li[role="presentation"]:first-of-type { display: ${value}; }`},
+  'Unread search results': function(value) {return `div.c-search_autocomplete li[role="presentation"]:first-of-type ~ .c-search_autocomplete__suggestion_item { display: ${value}; }`},
+  'Other search results': function(value) {return `div.c-search_autocomplete li[role="presentation"]:not(:first-of-type) ~ .c-search_autocomplete__suggestion_item { display: ${value}; }`},
+}
+
 function main (evt) {
   var sidebar_node = document.getElementsByClassName('p-channel_sidebar__list')[0];
 
   show_hide_button.addEventListener('click', function (evt) {
-    sidebar_node.style.visibility = hidden ? 'visible' : 'hidden';
+    target_visibility = hidden ? 'visible' : 'hidden';
+    target_display = hidden ? 'flex': 'none';
+
+    sidebar_node.style.visibility = target_visibility;
     show_hide_button.innerHTML = hidden ? 'Hide channels' : 'Show channels';
+
+    inject_css(selectors['Unread search results header'](target_display));
+    inject_css(selectors['Unread search results'](target_display));
+    inject_css(selectors['Other search results']('flex'));
+
     hidden = !hidden;
   });
 
